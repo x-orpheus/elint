b068969d3ca6af827844cc51c158cedf4ce2c043
keenwon <semanwmj@gmail.com>
2018-05-10 15:53:40 +0800

    基本的cli框架

--- bin/elint.js
+++ bin/elint.js
@@ -1,3 +1,34 @@
 #!/usr/bin/env node
 
 'use strict';
+
+const debug = require('debug')('elint:cli');
+const program = require('commander');
+const pkg = require('../package.json');
+
+debug('proces.argv: \n%O', process.argv);
+
+program
+  .usage('[input] [options]')
+  .description(pkg.description)
+  .version(pkg.version)
+  .action((...argus) => {
+    console.log(argus);
+  });
+
+program.parse(process.argv);
+
+function showHelp() {
+  // 没有输入任何参数
+  if (process.argv.length === 2) {
+    return true;
+  }
+
+  let argus = process.argv.slice(2);
+
+  return argus.every(item => item.startsWith('-'));
+}
+
+if (showHelp()) {
+  program.help();
+}
