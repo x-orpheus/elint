47f3e4c79074690b534341937fb762fcf85d9761
keenwon <semanwmj@gmail.com>
2018-05-15 22:23:59 +0800

    单元测试

---
+++ test/preset.specc.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { normalize } = require('../lib/preset');
+
+const mocha = require('mocha');
+const chai = require('chai');
+const should = chai.should();
+
+describe('Preset 测试', function () {
+
+  describe('# Normalize 测试', function () {
+
+    it('空测试', function () {
+      const name1 = undefined;
+      const name2 = '';
+
+      return should.not.exist(normalize(name1))
+        && normalize(name2).should.equal(name2);
+    });
+
+    it('正常情况', function () {
+      return normalize('name').should.equal('elint-preset-name')
+        && normalize('elint-preset-name').should.equal('elint-preset-name')
+        && normalize('elint-preset-na-me').should.equal('elint-preset-na-me');
+    });
+
+  });
+});
