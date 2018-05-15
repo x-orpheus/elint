bcdf6a35ee62f96de2d29556790bce7cc25b433b
keenwon <semanwmj@gmail.com>
2018-05-15 23:38:37 +0800

    添加 preset.verify 单元测试

--- test/mock.js
+++ test/mock.js
@@ -4,17 +4,12 @@ const path = require('path');
 
 let origin = process.cwd;
 
-function mock() {
+module.exports = function mock() {
   process.cwd = () => {
     return path.join(__dirname, 'test-project');
-  }
-}
+  };
 
-function unmock() {
-  process.cwd = origin;
-}
-
-module.exports = {
-  mock,
-  unmock
+  return () => {
+    process.cwd = origin;
+  };
 };
--- test/preset.specc.js
+++ test/preset.specc.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const { normalize } = require('../lib/preset');
+const unmock = require('./mock')();
+const { normalize, verify } = require('../lib/preset');
 
 const mocha = require('mocha');
 const chai = require('chai');
@@ -8,8 +9,9 @@ const should = chai.should();
 
 describe('Preset 测试', function () {
 
-  describe('# Normalize 测试', function () {
+  after(() => unmock);
 
+  describe('# Normalize 测试', function () {
     it('空测试', function () {
       const name1 = undefined;
       const name2 = '';
@@ -23,6 +25,17 @@ describe('Preset 测试', function () {
         && normalize('elint-preset-name').should.equal('elint-preset-name')
         && normalize('elint-preset-na-me').should.equal('elint-preset-na-me');
     });
-
   });
+
+  describe('# Verify 测试', function () {
+    it('不存在的 preset', function () {
+      return verify('name1').should.equal(false)
+        && verify('name2').should.equal(false);
+    });
+
+    it('存在的 preset', function () {
+      return verify('elint-preset-normal').should.equal(true);
+    });
+  });
+
 });
