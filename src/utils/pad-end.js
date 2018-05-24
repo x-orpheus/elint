70eaefac990f41f9ccdc9cf0590f96f887cd628f
keenwon <semanwmj@gmail.com>
2018-05-25 02:07:19 +0800

    add utils/pad-end.js

---
+++ src/utils/pad-end.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * polyfill String.prototype.padEnd
+ *
+ * @param {number} targetLength padding length
+ * @returns {string} string
+ */
+function polyfill(targetLength) {
+  const length = this.length;
+
+  if (targetLength <= length) {
+    return this;
+  }
+
+  return `${this}${' '.repeat(targetLength - length)}`;
+}
+
+/**
+ * String.prototype.padEnd
+ *
+ * @param {string} string string
+ * @param {number} targetLength padding length
+ * @returns {string} string
+ */
+function padEnd(string, targetLength) {
+  if (typeof string !== 'string'
+    || typeof targetLength !== 'number') {
+    return string;
+  }
+
+  const fn = typeof String.prototype.padEnd === 'function'
+    ? String.prototype.padEnd
+    : polyfill;
+
+  return fn.call(string, targetLength);
+}
+
+module.exports = padEnd;
