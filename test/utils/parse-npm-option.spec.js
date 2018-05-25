efbebeade7496cf230be4a33bda37715aa07d855
keenwon <semanwmj@gmail.com>
2018-05-25 20:19:47 +0800

    add parse-npm-option.js

---
+++ test/utils/parse-npm-option.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const parse = require('../../src/utils/parse-npm-option');
+
+const mocha = require('mocha');
+const chai = require('chai');
+const should = chai.should();
+
+describe('Parse npm option 测试', function () {
+  it('空测试', function () {
+    should.not.exist(parse());
+    parse('').should.be.equal('');
+  });
+
+  it('正常情况', function () {
+    parse('save').should.be.equal('--save');
+    parse('saveDev').should.be.equal('--save-dev');
+    parse('abCdeFgh').should.be.equal('--ab-cde-fgh');
+  });
+});
