4ca62d8613e002b44c5c91f253779051037916a1
keenwon <semanwmj@gmail.com>
2018-05-27 00:05:36 +0800

    add write-package-json.js

---
+++ src/utils/write-package-json.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const debug = require('debug')('elint:utils:writePackageJson');
+const path = require('path');
+const fs = require('fs-extra');
+const lodash = require('lodash');
+const writeJsonFile = require('write-json-file');
+const { baseDir } = require('../env');
+
+/**
+ * 将 devDependencies 写入 package.json
+ *
+ * @param {object} devDependencies 待写入的 devDependencies
+ * @returns {Promise} Promise
+ */
+function writePackageJson(devDependencies) {
+  debug('input devDependencies: %o', devDependencies);
+
+  if (!lodash.isPlainObject(devDependencies)) {
+    debug('devDependencies isn\'t plain object, return');
+    // 不报错
+    return Promise.resolve();
+  }
+
+  const pkgPath = path.join(baseDir, 'package.json');
+
+  debug(`package.json: ${pkgPath}`);
+
+  if (!fs.existsSync(pkgPath)) {
+    debug(`file not exists: ${pkgPath}, return`);
+    return Promise.resolve();
+  }
+
+  return writeJsonFile(pkgPath, devDependencies, {
+    detectIndent: true
+  });
+}
+
+module.exports = writePackageJson;
---
+++ test/utils/write-package-json.spec.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs-extra');
+const unmock = require('../mock')();
+const write = require('../../src/utils/write-package-json');
+const { baseDir } = require('../../src/env');
+
+const mocha = require('mocha');
+const chai = require('chai');
+const should = chai.should();
+
+const pkgPath = path.join(baseDir, 'package.json');
+const pkgContent = JSON.stringify({ name: 'package-name' }, null, '  ');
+
+describe('Write package json 测试', function () {
+
+  beforeEach(() => fs.writeFileSync(pkgPath, pkgContent));
+  afterEach(() => fs.removeSync(pkgPath));
+  after(() => unmock);
+
+  it('空测试', function (done) {
+    Promise.all([
+      write(''),
+      write()
+    ]).then(() => {
+      done();
+    });
+  });
+
+  it('package.json 不存在', function (done) {
+    fs.removeSync(pkgPath);
+
+    Promise.all([
+      write({ 'elint-preset-test': '*' })
+    ]).then(() => {
+      done();
+    });
+  });
+
+  it('正常测试', function (done) {
+    Promise.all([
+      write({ 'elint-preset-test': '*' })
+    ]).then(() => {
+      const content = fs.readFileSync(pkgPath);
+      const pkgData = JSON.parse(content);
+
+      if (pkgData['elint-preset-test'] === '*') {
+        done();
+      }
+    });
+  });
+});
