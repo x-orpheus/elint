cb543becec816e15f79e25db9909fa83ae4b278c
keenwon <semanwmj@gmail.com>
2018-05-29 23:49:38 +0800

    postinstall 部署 npm hook script

---
+++ src/postinstall.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const path = require('path');
+const fs = require('fs-extra');
+
+const cwd = process.cwd();
+const scriptPath = path.join(__dirname, '../scripts/postinstall');
+const destDirPath = path.join(cwd, 'node_modules/.hooks');
+const destScriptPath = path.join(destDirPath, 'postinstall');
+
+// 确保目录存在
+fs.ensureDirSync(destDirPath);
+
+// 部署 scripts
+fs.copyFileSync(scriptPath, destScriptPath);
+
+// 添加执行权限
+fs.chmodSync(destScriptPath, 755);
