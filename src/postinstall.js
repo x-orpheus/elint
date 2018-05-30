6a979d416f987aec6e090e737bd65101e0cdca5a
keenwon <semanwmj@gmail.com>
2018-05-31 00:44:31 +0800

    elint postinstall 时执行一次安装，保证任何安装顺序的可用

--- src/postinstall.js
+++ src/postinstall.js
@@ -2,16 +2,18 @@
 
 const path = require('path');
 const fs = require('fs-extra');
-const cwd = process.env.INIT_CWD;
+const tryRequire = require('./utils/try-require');
+const { nodeModulesDir } = require('./env');
+const { install } = require('./index');
 const pwd = process.env.PWD;
 
 // 开发过程中不执行
-if (!cwd || !pwd.includes('node_modules')) {
+if (!pwd.includes('node_modules')) {
   return;
 }
 
 const scriptPath = path.join(__dirname, '../scripts/postinstall');
-const destDirPath = path.join(cwd, 'node_modules/.hooks');
+const destDirPath = path.join(nodeModulesDir, '.hooks');
 const destScriptPath = path.join(destDirPath, 'postinstall');
 
 // 确保目录存在
@@ -22,3 +24,12 @@ fs.copySync(scriptPath, destScriptPath);
 
 // 添加执行权限
 fs.chmodSync(destScriptPath, 0o755);
+
+// 安装完成执行一次 install
+const presetName = tryRequire(/elint-preset-/)[0];
+
+if (!presetName) {
+  return;
+}
+
+install(presetName);
