41475f9b8854a75b62a21701efe4cb8157272be9
keenwon <semanwmj@gmail.com>
2018-06-09 17:05:09 +0800

    在临时目录执行单元测试

--- test/mock.js
+++ test/mock.js
@@ -1,13 +1,28 @@
 'use strict';
 
+const os = require('os');
 const path = require('path');
+const fs = require('fs-extra');
 
 let origin = process.env.INIT_CWD;
 
 module.exports = function mock() {
-  process.env.INIT_CWD = path.join(__dirname, 'test-project');
+  /**
+   * 创建测试项目
+   */
+  const tempDir = path.join(os.tmpdir(), `elint_tmp_${Date.now()}`);
+  const testProjectDir = path.join(__dirname, 'test-project');
+
+  fs.ensureDirSync(tempDir);
+  fs.copySync(testProjectDir, tempDir);
+
+  /**
+   * moch env.js baseDir
+   */
+  process.env.INIT_CWD = tempDir;
 
   return () => {
+    fs.removeSync(tempDir);
     process.env.INIT_CWD = origin;
   };
 };
