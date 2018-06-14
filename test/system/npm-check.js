3f7c00be82da9937416f94cbc8f09e2fff8eae1e
keenwon <semanwmj@gmail.com>
2018-06-14 16:35:29 +0800

    ci 监测有 bug 的 npm 版本，升级

---
+++ test/system/npm-check.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const execa = require('execa');
+
+/**
+ * 仅用于 ci，如果 npm 在 5.4 至 5.6 之间，升级 npm
+ */
+const version = execa.sync('npm', ['-v']).stdout;
+const versions = version.split('.');
+
+if (versions[0] !== '5' || !['4', '5', '6'].includes(versions[1])) {
+  return;
+}
+
+console.log('升级 npm');
+
+execa.sync('npm', ['install', 'npm', '-g']);
