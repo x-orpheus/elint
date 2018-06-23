3c25e227a870cde5be20b7f4ba661b8e93b111ba
keenwon <semanwmj@gmail.com>
2018-06-24 00:29:04 +0800

    fix: 调整设置 color 输出的方式，防止 eslint 报错

--- src/lib/exec.js
+++ src/lib/exec.js
@@ -4,8 +4,15 @@ const debug = require('debug')('elint:lib:exec');
 const execa = require('execa');
 
 const exec = pargram => (...argus) => {
-  debug(`run: ${pargram} ${argus.join(' ')} --color`);
-  return execa(pargram, [...argus, '--color']);
+  debug(`run: ${pargram} ${argus.join(' ')}`);
+
+  const env = Object.assign({}, process.env, {
+    FORCE_COLOR: true
+  });
+
+  return execa(pargram, [...argus], {
+    env
+  });
 };
 
 module.exports = exec;
