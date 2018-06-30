7f842bca1ee75308b42699961bd1b49b6b566e9b
keenwon <semanwmj@gmail.com>
2018-07-01 00:52:40 +0800

    test(system): 输出系统信息

--- test/system/init.js
+++ test/system/init.js
@@ -4,12 +4,18 @@
  * 执行一些准备工作
  */
 
+const os = require('os');
 const {
   run,
   npmCheck,
   elintPath,
   presetPath
 } = require('./utils');
+
+// 输出 CPU 和内存信息
+console.log('=== OS Info ===');
+console.log(`CPU: ${os.cpus().length}`);
+console.log(`Memory: ${Math.ceil(os.totalmem() / 1024 / 1024 / 1024)}G`)
 
 // 同步执行 elint 打包
 run('npm pack', elintPath, true);
