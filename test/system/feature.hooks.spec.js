103ffbaa671b23a97aea96492ab0c611aa51f5fd
keenwon <semanwmj@gmail.com>
2018-07-25 01:06:31 +0800

    test(system): 调整 hooks 测试，原来的 uninstall 测试并不生效

--- test/system/feature.hooks.spec.js
+++ test/system/feature.hooks.spec.js
@@ -17,6 +17,12 @@ beforeEach(function * (t) {
   yield run('git config user.name "zhang san"', tmpDir)
   yield run('git config user.email "zhangsan@gmail.com"', tmpDir)
 
+  /**
+   * 这里需要手动安装一次，因为 husky 的 postinstall 检查是 ci 环境，不执行安装
+   * 手动安装的时候，已经有了配置文件，配置文件 skipCI = false
+   */
+  yield run('npm run hooks-install', tmpDir)
+
   const hooksPath = path.join(tmpDir, '.git/hooks')
 
   t.context = {
@@ -43,11 +49,6 @@ test('hooks install && uninstall', function * (t) {
 test('lint commtest(error)', function * (t) {
   const { tmpDir } = t.context
 
-  /**
-   * 这里需要手动安装一次，因为 husky 的 postinstall 检查是 ci 环境，不执行安装
-   * 手动安装的时候，已经有了配置文件，配置文件 skipCI = false
-   */
-  yield run('npm run hooks-install', tmpDir)
   yield run('git add package.json', tmpDir)
 
   yield t.throws(run('git commit -m "hello"', tmpDir))
@@ -56,11 +57,6 @@ test('lint commtest(error)', function * (t) {
 test('lint commtest(success)', function * (t) {
   const { tmpDir } = t.context
 
-  /**
-   * 这里需要手动安装一次，因为 husky 的 postinstall 检查是 ci 环境，不执行安装
-   * 手动安装的时候，已经有了配置文件，配置文件 skipCI = false
-   */
-  yield run('npm run hooks-install', tmpDir)
   yield run('git add package.json', tmpDir)
 
   yield t.notThrows(run('git commit -m "build: hello"', tmpDir))
