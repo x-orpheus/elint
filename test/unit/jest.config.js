201944b896a15fecb3d50478e76029087e62e13e
keenwon <semanwmj@gmail.com>
2020-09-30 09:53:44 +0800

    test: 延长超时时间，防止 windows ci 报错

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testMatch: [
     '**/test/unit/**/*.spec.js'
   ],
+  testTimeout: 5 * 60 * 1000,
   setupFilesAfterEnv: ['jest-extended'],
   collectCoverage: true,
   coverageDirectory: '../../coverage'
