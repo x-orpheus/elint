7a84673b8e05abdbc12b12514da09e82daa26ab1
iwyvi <iwyvi@outlook.com>
2020-11-13 11:19:59 +0800

    feat: 全局导出 prettierlint

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ exports.commitlint = require('./workers/commitlint')
 exports.runHooks = require('./hooks')
 
 exports.version = require('./version')
+
+exports.prettierlint = require('./workers/prettier/lint')
