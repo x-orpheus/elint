df1b6d66ace253ca15353ccb174915a06d3b38ec
iwyvi <iwyvi@outlook.com>
2022-04-12 16:36:27 +0800

    feat(prettier): 支持 .prettierignore

--- packages/elint-plugin-prettier/src/index.ts
+++ packages/elint-plugin-prettier/src/index.ts
@@ -1,9 +1,28 @@
+import path from 'path'
 import { createRequire } from 'module'
+import type { Ignore } from 'ignore'
 import prettier, { type Options } from 'prettier'
 import type { ElintPlugin, ElintPluginResult } from 'elint'
 
 const require = createRequire(import.meta.url)
 const { clearConfigCache, resolveConfig, format } = prettier
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const { createIgnorer } = prettier.__internal
+
+let ignorerMap: Record<string, Ignore> = {}
+
+const getIgnorer = (cwd: string) => {
+  let ignorer = ignorerMap[cwd]
+
+  if (!ignorer) {
+    ignorer = createIgnorer.sync(path.join(cwd, '.prettierignore'))
+    ignorerMap[cwd] = ignorer
+  }
+
+  return ignorer
+}
 
 // 使用 prettier 的方法获取当前文件的格式化配置
 const getOptionsForFile = (filePath: string) => {
@@ -45,6 +64,12 @@ const elintPluginPrettier: ElintPlugin<never> = {
       success: true
     }
 
+    const ignorer = getIgnorer(cwd)
+
+    if (filePath && ignorer.ignores(filePath)) {
+      return result
+    }
+
     const options = getOptionsForFile(filePath || cwd)
 
     const formatted = format(text, options)
@@ -66,6 +91,7 @@ const elintPluginPrettier: ElintPlugin<never> = {
     }
   },
   reset() {
+    ignorerMap = {}
     clearConfigCache()
   }
 }
