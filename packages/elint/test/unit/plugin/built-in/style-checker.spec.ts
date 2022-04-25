9e5e68c964677a11f6812a10b794bfe9463b646c
iwyvi <iwyvi@outlook.com>
2022-04-25 19:35:37 +0800

    test: 添加 style-checkre 单元测试

---
+++ packages/elint/test/unit/plugin/built-in/style-checker.spec.ts
@@ -0,0 +1,67 @@
+import chalk from 'chalk'
+import styleChecker from '../../../../src/plugin/built-in/style-checker.js'
+import { testElintPlugin } from '../../../../src/plugin/test.js'
+
+describe('样式检查插件测试', () => {
+  test('跳过样式检查', async () => {
+    const result = await testElintPlugin('test1', styleChecker, {
+      fix: false,
+      style: false,
+      source: 'test1',
+      cwd: ''
+    })
+
+    expect(result).toBeUndefined()
+  })
+
+  test('代码一致', async () => {
+    const result = await testElintPlugin('test1', styleChecker, {
+      fix: false,
+      style: true,
+      source: 'test1',
+      cwd: ''
+    })
+
+    expect(result?.errorCount).toBe(0)
+    expect(result?.message).toBeUndefined()
+  })
+
+  test('代码不一致', async () => {
+    const result = await testElintPlugin('test1', styleChecker, {
+      fix: false,
+      style: true,
+      source: 'test2',
+      cwd: ''
+    })
+
+    expect(result?.errorCount).toBe(1)
+    expect(result?.message).toBe(`${chalk.red.bold('!')} Not formatted\n\n`)
+  })
+
+  test('代码不一致，传入 filePath', async () => {
+    const result = await testElintPlugin('test1', styleChecker, {
+      fix: false,
+      style: true,
+      source: 'test2',
+      cwd: '',
+      filePath: 'filePath'
+    })
+
+    expect(result?.message).toBe(
+      `${chalk.underline('filePath')}\n  ${chalk.red.bold(
+        '!'
+      )} Not formatted\n\n`
+    )
+  })
+
+  test('fix 模式代码不一致', async () => {
+    const result = await testElintPlugin('test1', styleChecker, {
+      fix: true,
+      style: true,
+      source: 'test2',
+      cwd: ''
+    })
+
+    expect(result?.errorCount).toBe(0)
+  })
+})
