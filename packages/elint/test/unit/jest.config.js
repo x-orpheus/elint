b88214543465fe82ac463a9b44b11a8695fc2052
iwyvi <iwyvi@outlook.com>
2022-05-20 10:31:53 +0800

    test: 修复升级 jest 后单元测试配置

--- packages/elint/test/unit/jest.config.js
+++ packages/elint/test/unit/jest.config.js
@@ -1,3 +1,8 @@
+import path from 'path'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+
 export default {
   rootDir: '../..',
   preset: 'ts-jest',
@@ -9,9 +14,16 @@ export default {
   },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
+    // jest 28 识别了 exports 字段，但是没有识别 imports 字段
     // for chalk
-    '#ansi-styles': 'chalk/source/vendor/ansi-styles/index.js',
-    '#supports-color': 'chalk/source/vendor/supports-color/index.js'
+    '#ansi-styles': path.join(
+      require.resolve('chalk').split('chalk/')[0],
+      'chalk/source/vendor/ansi-styles/index.js'
+    ),
+    '#supports-color': path.join(
+      require.resolve('chalk').split('chalk/')[0],
+      'chalk/source/vendor/supports-color/index.js'
+    )
   },
   testMatch: ['**/test/unit/**/*.spec.ts'],
   testTimeout: 5 * 60 * 1000,
