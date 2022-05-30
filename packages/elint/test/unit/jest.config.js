cfddcd4db1d4b9102ff6b904a905f9c2b72731bf
iwyvi <iwyvi@outlook.com>
2022-05-30 11:17:34 +0800

    test(unit): 调整 Windows 下的 moduleNameMapper

--- packages/elint/test/unit/jest.config.js
+++ packages/elint/test/unit/jest.config.js
@@ -1,4 +1,5 @@
 import path from 'path'
+import { pathToFileURL } from 'url'
 import { createRequire } from 'module'
 
 const require = createRequire(import.meta.url)
@@ -16,14 +17,20 @@ export default {
     '^(\\.{1,2}/.*)\\.js$': '$1',
     // jest 28 识别了 exports 字段，但是没有识别 imports 字段
     // for chalk
-    '#ansi-styles': path.join(
-      require.resolve('chalk').split('chalk/')[0],
-      'chalk/source/vendor/ansi-styles/index.js'
+    '#ansi-styles': pathToFileURL(
+      path
+        .join(
+          require.resolve('chalk').split('chalk/')[0],
+          'chalk/source/vendor/ansi-styles/index.js'
+        )
+        .toString()
     ),
-    '#supports-color': path.join(
-      require.resolve('chalk').split('chalk/')[0],
-      'chalk/source/vendor/supports-color/index.js'
-    )
+    '#supports-color': pathToFileURL(
+      path.join(
+        require.resolve('chalk').split('chalk/')[0],
+        'chalk/source/vendor/supports-color/index.js'
+      )
+    ).toString()
   },
   testMatch: ['**/test/unit/**/*.spec.ts'],
   testTimeout: 5 * 60 * 1000,
