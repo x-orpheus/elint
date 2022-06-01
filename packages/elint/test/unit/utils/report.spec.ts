50b3f89338ef2c8a3c10eabb305a9f9ad2bbc809
iwyvi <iwyvi@outlook.com>
2022-06-01 10:19:17 +0800

    test(unit): 完善 report 覆盖

--- packages/elint/test/unit/utils/report.spec.ts
+++ packages/elint/test/unit/utils/report.spec.ts
@@ -113,4 +113,88 @@ describe('report 测试', () => {
 
     expect(report(input)).toEqual(result.join(''))
   })
+
+  test('警告输出', () => {
+    const input: ElintResult[] = [
+      {
+        source: '',
+        output: '',
+        errorCount: 0,
+        warningCount: 2,
+        pluginResults: [
+          {
+            errorCount: 0,
+            source: '',
+            output: '',
+            warningCount: 2,
+            message: 'output\n',
+            pluginData: {
+              name: 'test',
+              title: 'name',
+              type: 'linter'
+            }
+          }
+        ]
+      }
+    ]
+
+    const result = []
+
+    result.push('\n')
+    result.push(`${chalk.bold('> name:')}\n`)
+    result.push('\n')
+    result.push('  output\n')
+    result.push('\n')
+    result.push(`${chalk.bold('> elint:')}\n`)
+    result.push('\n')
+    result.push(
+      '  ' + chalk.yellow(`${figures.cross} 2 problems (0 error, 2 warnings)`)
+    )
+    result.push('\n')
+    result.push('\n')
+
+    expect(report(input)).toEqual(result.join(''))
+  })
+
+  test('失败输出', () => {
+    const input: ElintResult[] = [
+      {
+        source: '',
+        output: '',
+        errorCount: 1,
+        warningCount: 2,
+        pluginResults: [
+          {
+            errorCount: 1,
+            source: '',
+            output: '',
+            warningCount: 2,
+            message: 'output\n',
+            pluginData: {
+              name: 'test',
+              title: 'name',
+              type: 'linter'
+            }
+          }
+        ]
+      }
+    ]
+
+    const result = []
+
+    result.push('\n')
+    result.push(`${chalk.bold('> name:')}\n`)
+    result.push('\n')
+    result.push('  output\n')
+    result.push('\n')
+    result.push(`${chalk.bold('> elint:')}\n`)
+    result.push('\n')
+    result.push(
+      '  ' + chalk.red(`${figures.cross} 3 problems (1 error, 2 warnings)`)
+    )
+    result.push('\n')
+    result.push('\n')
+
+    expect(report(input)).toEqual(result.join(''))
+  })
 })
