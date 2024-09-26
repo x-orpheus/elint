ea32d1b126bceaf4603a6d9db124059a02980d08
iwyvi <iwyvi@outlook.com>
2024-09-26 18:07:13 +0800

    feat: 新增 husky 插件

---
+++ packages/plugin-husky/README.md
@@ -0,0 +1,19 @@
+# @elint/plugin-husky
+
+elint 的 [husky](https://github.com/typicode/husky) 插件
+
+## 配置 preset
+
+1. 安装依赖 `@elint/plugin-husky`、`husky`
+
+2. 配置 `index.js`
+
+```javascript
+module.exports = {
+  plugins: ['@elint/plugin-husky']
+}
+```
+
+3. 添加配置文件
+
+https://typicode.github.io/husky/how-to.html
---
+++ packages/plugin-husky/src/index.ts
@@ -0,0 +1,37 @@
+import type Husky from 'husky'
+import { defineElintPlugin } from 'elint'
+import { ElintPluginType } from 'packages/elint/src/index.js'
+
+let husky: typeof Husky | undefined
+
+const elintPluginHusky = defineElintPlugin<null>({
+  name: '@elint/plugin-husky',
+  title: 'Husky',
+  type: ElintPluginType.Common,
+  configFiles: ['.husky'],
+  activateConfig: {
+    activate() {
+      return false
+    }
+  },
+  async load(ctx, importFromPreset) {
+    husky = await importFromPreset('husky')
+  },
+  async prepare(ctx) {
+    if (husky) {
+      husky(ctx.cwd)
+    } else {
+      throw new Error('husky is not installed')
+    }
+  },
+  async execute(text) {
+    return {
+      source: text,
+      output: text,
+      errorCount: 0,
+      warningCount: 0
+    }
+  }
+})
+
+export default elintPluginHusky
